ed6e4c6f131290235766f80eebffdfe6f7f875ad
Samantha G <antashma@gmail.com>
2025-03-25 21:49:17 -0400

    added font awesome

--- google-api/google-login/frontend/src/App.jsx
+++ google-api/google-login/frontend/src/App.jsx
@@ -4,9 +4,9 @@ function App() {
 
   return (
     <>
-     <h1>FocusTask</h1>
+     <h1><i class="fa-solid fa-list-check"></i> FocusTask</h1>
      <p>Create a goal and make progress today.</p>
-     <a href="http://localhost:8000/auth/google">Login with Google</a>
+     <a href="http://localhost:8000/auth/google"><i className="fa-brands fa-google"></i> Login with Google</a>
     </>
   )
 }
