9ba1a53db43550b106d93f31e6f6651ee61a9b89
Samantha G <antashma@gmail.com>
2025-03-25 22:28:39 -0400

    storybooks -- can login/register with google :)

--- google-api/storybooks/config/passport.js
+++ google-api/storybooks/config/passport.js
@@ -11,6 +11,25 @@ module.exports = function(passport) {
         },
         async (accessToken, refreshToken, profile, done) => {
             console.log(profile);
+            const newUser = {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value
+            }
+
+            try {
+                let user = await User.findOne({googleId: profile.id});
+                if (user) {
+                    done(null, user);
+                } else {
+                    user = await User.create(newUser);
+                    done(null, user);
+                }
+            } catch (error) {
+                console.log(error);
+            }
         })
     )
 
@@ -19,6 +38,7 @@ module.exports = function(passport) {
     });
 
     passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => done(err, user.id));
+        User.findById(id)
+            .then(user => done(null, user.id))
     })
 }
